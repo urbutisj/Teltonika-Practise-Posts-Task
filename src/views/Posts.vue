1d0606ef562c68137416a944549f69e873285323
Justinas <urbutisj@gmail.com>
2022-09-29 15:03:19 +0300

    Added Pagination

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -4,8 +4,11 @@
             <h1 class="title">Straipsniai</h1>
             <input type="text" v-model="search" class="input mb-3" placeholder="Straipsnių paiešką..."/>
             <div class="columns" v-if="posts.length > 0">
-                <div class="column post is-half" v-for = "post of filteredBlogs" :key="post.id">
+                <div class="column post is-half" v-for = "post in pageOfItems" :key="post.id">
                     <post class="box" :fetchPosts="fetchPosts"  :post="post" v-on:changeState="updateEditModalState" v-on:postId="updateEditModalState"/>
+                </div>
+                <div class="pagination-row">
+                    <app-pagination :items="filteredBlogs" :pageSize="10" @changePage="onChangePage"></app-pagination>
                 </div>
             </div>
             <div v-else>
@@ -18,18 +21,27 @@
 </template>
 
 <script>
-    
+    import JwPagination from 'jw-vue-pagination';
     import Post from '../components/Post.vue';
     import projectMixin from '../mixins/projectMixin';
     export default {
         name: 'Posts',
         props: ['updateEditModalState', 'fetchPosts', 'posts'],
         components: {
-            'post' : Post
+            'post' : Post,
+            'app-pagination': JwPagination
     },
     data() {
         return {
             search: '',
+            pageOfItems: []
+        }
+    },
+    methods: {
+        onChangePage(pageOfItems) {
+            // update page of items
+            this.pageOfItems = pageOfItems;
+            console.log(this.pageOfItems);
         }
     },
     async mounted() {
@@ -47,5 +59,13 @@
 }
 .column {
     flex: 0 0 50%;
+}
+
+.pagination-row {
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    margin-top: 3rem;
+    display: flex;
 }
 </style>
