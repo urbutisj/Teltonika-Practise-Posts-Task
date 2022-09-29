573bc1774dc9f0a6aeb329117a03f76761adff43
Justinas <urbutisj@gmail.com>
2022-09-29 15:23:27 +0300

    Created 404 View

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 import Posts from "./views/Posts.vue"
 import SinglePostPage from './views/SinglePostPage.vue'
+import PageNotFound from './views/PageNotFound.vue'
 
 export default [
     {path: "/", component: Posts},
-    {path: "/blog-post/:id", component: SinglePostPage}
+    {path: "/blog-post/:id", component: SinglePostPage},
+    {path: "*", component: PageNotFound }
 ]
---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,26 @@
+<template>
+    <section class="section">
+        <div class="container not-found">
+            <h1 class="title">Not Found 404</h1>
+        </div>
+    </section> 
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    section{
+        height: 40vh;
+    }
+
+    .container.not-found{
+        display: flex;
+        justify-content: center;
+        align-items: center;;
+        text-align: center;
+        height: 100%;
+    }
+    
+</style>
