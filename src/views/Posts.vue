1d34706bea609b3af1ce89b40de8f6c49b632986
Justinas <urbutisj@gmail.com>
2022-10-03 09:02:09 +0300

    Changed Pagination Labels

--- src/views/Posts.vue
+++ src/views/Posts.vue
@@ -17,7 +17,9 @@
                     <app-pagination 
                         :items="filteredBlogs" 
                         :pageSize="10"
-                        @changePage="onChangePage">
+                        @changePage="onChangePage"
+                        :labels="customLabels"
+                        >
                     </app-pagination>
                 </div>
             </div>
@@ -34,6 +36,14 @@
     import JwPagination from 'jw-vue-pagination';
     import Post from '../components/Post.vue';
     import projectMixin from '../mixins/projectMixin';
+
+    const customLabels = {
+                first: "<<",
+                last: ">>",
+                previous: "<",
+                next: ">"
+    };
+
     export default {
         name: 'Posts',
         props: ['updateEditModalState', 'fetchPosts', 'posts'],
@@ -44,7 +54,9 @@
     data() {
         return {
             search: '',
-            pageOfItems: []
+            pageOfItems: [],
+            customLabels
+            
         }
     },
     methods: {
@@ -54,6 +66,7 @@
     },
     async mounted() {
         await this.fetchPosts();
+        
     },
     mixins: [projectMixin ]
     
@@ -74,5 +87,30 @@
     justify-content: center;
     margin-top: 3rem;
     display: flex;
+}
+
+.page-item:first-child .page-link {
+  border-top-left-radius: .25rem;
+  border-bottom-left-radius: .25rem;
+}
+
+.page-item:last-child .page-link {
+  border-top-right-radius: .25rem;
+  border-bottom-right-radius: .25rem;
+}
+
+.page-link{
+    border: 1px solid #dee2e6;
+}
+
+.page-item.disabled .page-link {
+  color: #6c757d;
+  border-color: #dee2e6;
+}
+
+.page-item.active .page-link {
+  color: #fff;
+  background-color: #007bff;
+  border-color: #007bff;
 }
 </style>
